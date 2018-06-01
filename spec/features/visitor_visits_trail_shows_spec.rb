e06c0130cbe843d416d2583d0f77c1214dc45951
zdcthomas <zdcthomas@yahoo.com>
2018-06-01 10:19:42 -0600

    implements 9th US

--- spec/features/visitor_visits_trail_shows_spec.rb
+++ spec/features/visitor_visits_trail_shows_spec.rb
@@ -8,13 +8,14 @@ RSpec.feature "VisitorVisitsTrailShows", type: :feature do
         @trip2 = Trip.create!(name:'another trip')
         @trip3 = Trip.create!(name:'wow, more trips')
         @trip4 = Trip.create!(name:'hey another trip')
-        @trail = @trip1.trails.create!(length:3, name:'happy',address:'123 asdf')
+        @trail = @trip1.trails.create!(length:3, name:'another trail', address:'123 asdf')
         TripTrail.create!(trip_id:@trip2.id, trail_id:@trail.id)
         TripTrail.create!(trip_id:@trip3.id, trail_id:@trail.id)
         TripTrail.create!(trip_id:@trip4.id, trail_id:@trail.id)
 
         visit trail_path(@trail)
 
+        expect(page).to have_content(@trail.address)
         @trail.trips.each do |trip|
           expect(page).to have_content(trip.total_trail_length)
           expect(page).to have_content(trip.name)
